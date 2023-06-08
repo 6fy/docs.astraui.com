f18ea5a9cf36585111c2bef645248605fd446a29
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-06-08 14:16:48 +0300

    add: Menu

---
+++ pages/components/ToggleMenu.tsx
@@ -0,0 +1,28 @@
+import { Menu, MenuItem, Button, Box } from "astra-ui-kit";
+import { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+export const ToggleMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Button
+        onClick={handleToggle}
+        endIcon={isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}>
+        Toggle Menu
+      </Button>
+      {isOpen && (
+        <Menu sx={{ color: "black" }}>
+          <MenuItem onClick={() => setIsOpen(false)}>Menu Item 1</MenuItem>
+          <MenuItem onClick={() => setIsOpen(false)}>Menu Item 2</MenuItem>
+          <MenuItem onClick={() => setIsOpen(false)}>Menu Item 3</MenuItem>
+        </Menu>
+      )}
+    </>
+  );
+};
